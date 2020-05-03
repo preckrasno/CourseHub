6a610fdeaab1815580b356dedab62e5b533a6aaf
Dmytro Muntian <dmytro.muntian@ubisft.com>
2020-05-03 21:49:21 +0300

    Add view, template for hub post

--- coursehub_back/settings.py
+++ coursehub_back/settings.py
@@ -14,6 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+print(BASE_DIR)
 
 
 # Quick-start development settings - unsuitable for production
@@ -118,5 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/hub/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'hub\\static')
+print('STATIC_URL =', STATIC_URL)
+print('STATIC_ROOT =', STATIC_ROOT)
